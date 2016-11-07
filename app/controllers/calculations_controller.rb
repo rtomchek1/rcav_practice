6d1e47b38ab915f861c4e9b7fadfcb52f0fcbece
Ryan Tomchek <ryan.tomchek@chicagobooth.edu>
2016-11-07 11:16:05 -0600

    HW submission

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,28 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @num = params[:num]
+    @num_squared = @num.to_i * @num.to_i
+  end
+
+  def square_root
+    @num = params[:num]
+    @num_sqrt = sprintf('%.2f',Math.sqrt(@num.to_i))
+  end
+
+  def random_num
+    @min = params[:min]
+    @max = params[:max]
+    @random_num = (rand * (@max.to_i - @min.to_i) + @min.to_i).round(0)
+  end
+
+  def payment
+    @int_rate = params[:int_rate]
+    @duration = params[:duration]
+    @amount = params[:amount]
+
+    @payment = sprintf('%.2f',@amount.to_i * ((@int_rate.to_f/10000)/12) * (1+(@int_rate.to_f/10000)/12)**(@duration.to_i*12) / ((1+(@int_rate.to_f/10000)/12)**(@duration.to_i*12)-1))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square/:num", {:controller => "calculations", :action => "square"})
+  get("/square_root/:num", {:controller => "calculations", :action => "square_root"})
+  get("/random/:min/:max", {:controller => "calculations", :action => "random_num"})
+  get("/payment/:int_rate/:duration/:amount", {:controller => "calculations", :action => "payment"})
 end
